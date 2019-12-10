afc77f964c009baa695162a2689bfb6af4914bc6
Ernest Murray <ejmurray247@hotmail.com>
2019-12-10 08:05:39 +0000

    Added solutions and tested.

--- 005_parse_list_of_names/names.py
+++ 005_parse_list_of_names/names.py
@@ -1,3 +1,5 @@
+import snoop
+
 NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
          'julian sequeira', 'sandra bullock', 'keanu reeves',
          'julbob pybites', 'bob belderbos', 'julian sequeira',
@@ -6,16 +8,20 @@ NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
 
 def dedup_and_title_case_names(names):
     """Should return a list of names, each name appears only once"""
-    pass
+    return list(set([name.title() for name in names]))
 
 
 def sort_by_surname_desc(names):
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
-    # ...
+    return sorted(names, key=lambda x: x.split(" ")[-1], reverse=True)
 
 
 def shortest_first_name(names):
     """Returns the shortest first name (str)"""
     names = dedup_and_title_case_names(names)
-    # ...
+    first_names = []
+    for name in names:
+        first_names.append(name.split(" ")[0])
+
+    return min((first_name for first_name in first_names), key=len)
